51cbd78c123aa18bc30576c06e2c4028e778bd0a
Vadim Kovshov <ssevenn41@gmail.com>
2024-02-01 08:48:46 +0200

    created view with "GET/POST" methods

--- hand_made_project/crafter/views.py
+++ hand_made_project/crafter/views.py
@@ -1,3 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import CrafterPerson
+# from .serializers import CrafterSerializer
+
+
+# class CrafterAPIView(generics.ListAPIView):
+#     queryset = CrafterPerson.objects.all()
+#     serializer_class = CrafterSerializer
+
+
+class CrafterAPIView(APIView):
+    def get(self, request):
+        lst = CrafterPerson.objects.all().values()
+        return Response({'crafters': list(lst)})
+
+    def post(self, request):
+        crafter_new = CrafterPerson.objects.create(
+            name=request.data['name'],
+            phone_number=request.data['phone_number'],
+            category_id=request.data['category_id']
+        )
+        return Response({'crafter': model_to_dict(crafter_new)})
--- hand_made_project/hand_made_project/urls.py
+++ hand_made_project/hand_made_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from crafter.views import CrafterAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/crafter_list', CrafterAPIView.as_view())
 ]
