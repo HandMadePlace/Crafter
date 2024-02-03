a265a962f179e7c2d7986343a68c950f7ec671a0
Vadim Kovshov <ssevenn41@gmail.com>
2024-02-03 22:19:34 +0200

    Added methods for creating and updating objects

--- hand_made_project/crafter/serializers.py
+++ hand_made_project/crafter/serializers.py
@@ -8,3 +8,14 @@ class CrafterSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     phone_number = serializers.CharField(max_length=50)
     category_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return CrafterPerson.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.category_id = validated_data.get('category_id', instance.category_id)
+        instance.save()
+
+        return instance
