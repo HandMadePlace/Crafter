f8cd5be87720d5e1effe3be730433c2a2c5979ff
Vadim Kovshov <ssevenn41@gmail.com>
2024-02-03 19:21:05 +0200

    Changed view using serializer and added incoming data validation on post request

--- hand_made_project/crafter/views.py
+++ hand_made_project/crafter/views.py
@@ -1,11 +1,10 @@
-from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import CrafterPerson
-# from .serializers import CrafterSerializer
+from .serializers import CrafterSerializer
 
 
 # class CrafterAPIView(generics.ListAPIView):
@@ -15,13 +14,16 @@ from .models import CrafterPerson
 
 class CrafterAPIView(APIView):
     def get(self, request):
-        lst = CrafterPerson.objects.all().values()
-        return Response({'crafters': list(lst)})
+        lst = CrafterPerson.objects.all()
+        return Response({'crafters': CrafterSerializer(lst, many=True).data})
 
     def post(self, request):
+        serializer = CrafterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         crafter_new = CrafterPerson.objects.create(
             name=request.data['name'],
             phone_number=request.data['phone_number'],
             category_id=request.data['category_id']
         )
-        return Response({'crafter': model_to_dict(crafter_new)})
+        return Response({'crafter': CrafterSerializer(crafter_new).data})
