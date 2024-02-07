326133ed6d28fd30a88eed0359ff11cdc83e6fe5
Vadim Kovshov <ssevenn41@gmail.com>
2024-02-07 22:14:58 +0200

    Added a custom path function to create a custom path to display categories

--- hand_made_project/crafter/views.py
+++ hand_made_project/crafter/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
-from .models import CrafterPerson
+from .models import CrafterPerson, CategoryCrafter
 from .serializers import CrafterSerializer
 
 
 class CrafterPersonViewSet(viewsets.ModelViewSet):
     queryset = CrafterPerson.objects.all()
     serializer_class = CrafterSerializer
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None) -> Response:
+        category = CategoryCrafter.objects.get(pk=pk)
+
+        return Response({'category': category.title})
