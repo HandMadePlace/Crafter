d9cf0c007c1e3d74d0283da951bf41baacf84f19
Vadim Kovshov <ssevenn41@gmail.com>
2024-02-07 21:55:58 +0200

    Added a "user" field to CrafterPerson to verify authentication

--- hand_made_project/crafter/models.py
+++ hand_made_project/crafter/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,6 +6,7 @@ class CrafterPerson(models.Model):
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=50)
     category = models.ForeignKey(to='CategoryCrafter', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, verbose_name='Користувач', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
